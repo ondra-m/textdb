1d0292b5a08363c2e188167bdc8075002453989f
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2013-07-15 21:44:31 +0200

    Update spec for listen.

--- spec/lib/04_update_spec.rb
+++ spec/lib/04_update_spec.rb
@@ -1,8 +1,11 @@
 require "spec_helper"
 
 describe "Textdb.update" do
+  let(:base_folder)   { Textdb.config.base_folder }
+  let(:data_file_ext) { Textdb.config.data_file_extension }
+
   it "create with update" do
-    file = File.join RSpec.configuration.dir, "key3", "value3_1#{Textdb.config.data_file_extension}"
+    file = File.join base_folder, "key3", "value3_1#{data_file_ext}"
 
     expect( File.exist?(file) ).to eql(false)
 
@@ -14,11 +17,28 @@ describe "Textdb.update" do
   end
 
   it "update" do
-    file = File.join RSpec.configuration.dir, "key3", "value3_1#{Textdb.config.data_file_extension}"
+    file = File.join base_folder, "key3", "value3_1#{data_file_ext}"
 
     Textdb.update("text 2") { key3.value3_1 }
 
     expect( File.read(file) ).to eql("text 2")
   end
 
+  context "listen" do
+    it "update a file" do
+      Textdb.create { key3.value3_2 }.update("text")
+
+      sleep 0.5
+
+      expect( Textdb.read { key3.value3_2 } ).to eql("text")
+
+      file = File.join Textdb.config.base_folder, "key3", "value3_2#{data_file_ext}"
+
+      File.open(file, 'w') { |f| f.write("text 2") }
+
+      sleep 0.5
+
+      expect( Textdb.read { key3.value3_2 } ).to eql("text 2")
+    end
+  end
 end
